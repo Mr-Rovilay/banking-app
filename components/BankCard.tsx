3ba399e6ca4c5cb3326b698ef51a5418d9c6b9d8
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-08-19 15:06:37 +0100

    mk

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -9,14 +9,14 @@ const BankCard = ({account, userName, showBalance = true}: CreditCardProps) => {
         <Link href={"/"} className='bank-card'>
         <div className="bank-card_content">
             <div className="">
-                <h1 className='text-16 font-semibold text-white'>
+                <h1 className='text-16 font-semibold text-white capitalize'>
                     {account.name || userName}
                 </h1>
                 <p className="font-ibm-plex-serif font-black text-white">{formatAmount(account.currentBalance)}</p>
             </div>
             <article className='flex flex-col gap-2'>
               <div className="flex justify-between">
-                <h1 className='text-12 font-semibold text-white'>{userName}</h1>
+                <h1 className='text-12 font-semibold text-white capitalize'>{userName}</h1>
                 <h2 className='text-12 font-semibold text-white'>●● / ●●</h2>
               </div>
               <p className="text-14 font-semibold tracking-[1.1px] text-white">●●●● ●●●● ●●●●<span className='text-16'>{account.mask} 1234</span> </p>
